036d373d5474482eab40db52c9df47b50746e51b
Danib <danib@hotmail.com>
2021-11-15 16:02:05 +0100

    Quiz responsive layout

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// test test
+/**
+ * 
+ * 
+ */
+
+const buttonButtonEl = document.querySelector('#button-button');
+const resetButtonEl = document.querySelector('#reset-button')
+const answerButtonEl = document.querySelector('#answer')
+
+answerButtonEl.addEventListener('click', e => {
+    console.log('My answer')
+});
+
+buttonButtonEl.addEventListener('click', e => {
+    console.log('Nothing happened');
+});
+
+resetButtonEl.addEventListener('click', e => {
+    console.log('You tried to reset the game');
+});
